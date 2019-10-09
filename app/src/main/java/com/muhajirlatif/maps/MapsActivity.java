dc8f136f9ccf1983272d2948f6ef57a52cb31f7e
haimax <muhajir16@mhs.if.its.ac.id>
2019-10-09 23:58:19 +0700

    benerin tampilan

--- app/src/main/java/com/muhajirlatif/maps/MapsActivity.java
+++ app/src/main/java/com/muhajirlatif/maps/MapsActivity.java
@@ -1,10 +1,19 @@
 package com.muhajirlatif.maps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,8 +21,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,66 +33,49 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
 
+    private static final int TAG_CODE_PERMISSION_LOCATION = 1;
     private EditText etLatitude, etLongitude, etZoom, etAddress;
     private ImageButton btnGo, btnSearch;
+
+    private LinearLayout bottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
 
     private GoogleMap mMap;
     private Marker marker;
     private float zoom = 15f;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item_menu_normal:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                break;
+    private TextView tvLatitude, tvLongitude;
 
-            case R.id.item_menu_hybrid:
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                break;
-
-            case R.id.item_menu_terrain:
-                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                break;
-
-            case R.id.item_menu_satellite:
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                break;
-
-            case R.id.item_menu_none:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    private LocationManager locationManager;
+    private Location currentLocation;
+    private MyLocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        etLatitude = findViewById(R.id.etLatitude);
-        etLongitude = findViewById(R.id.etLongitude);
-        etZoom = findViewById(R.id.etZoom);
-        etAddress = findViewById(R.id.etAddress);
-        btnGo = findViewById(R.id.btnGo);
-        btnSearch = findViewById(R.id.btnSearch);
+        initView();
+        initOnClick();
+        checkLocationPermission();
 
-        btnGo.setOnClickListener(this);
-        btnSearch.setOnClickListener(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 200, 10, locationListener);
+
+        // Init current location
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        tvLatitude.setText(String.valueOf(currentLocation.getLatitude()));
+        tvLongitude.setText(String.valueOf(currentLocation.getLongitude()));
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -88,6 +83,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    private void initView() {
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+        tvLatitude = findViewById(R.id.tvLatitude);
+        tvLongitude = findViewById(R.id.tvLongitude);
+
+        etLatitude = findViewById(R.id.etLatitude);
+        etLongitude = findViewById(R.id.etLongitude);
+        etZoom = findViewById(R.id.etZoom);
+        etAddress = findViewById(R.id.etAddress);
+        btnGo = findViewById(R.id.btnGo);
+        btnSearch = findViewById(R.id.btnSearch);
+    }
+
+    private void initOnClick() {
+        btnGo.setOnClickListener(this);
+        btnSearch.setOnClickListener(this);
+    }
+
+    private void checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[] {
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION },
+                        TAG_CODE_PERMISSION_LOCATION);
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -102,10 +127,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Current Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoom));
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), zoom));
     }
 
     public void searchAddress(String address) {
@@ -167,6 +191,64 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 if (!address.isEmpty()) {
                     searchAddress(address);
                 }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_menu_normal:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+
+            case R.id.item_menu_hybrid:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            case R.id.item_menu_terrain:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+
+            case R.id.item_menu_satellite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+
+            case R.id.item_menu_none:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            currentLocation = location;
+            tvLatitude.setText(String.valueOf(currentLocation.getLatitude()));
+            tvLongitude.setText(String.valueOf(currentLocation.getLongitude()));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
         }
     }
 }
